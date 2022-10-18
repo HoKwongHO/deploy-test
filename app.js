cebb3e5ec523151af53e678f07d4f9485cd02c42
HoKwongHO <hokwongh@student.unimelb.edu.au>
2022-10-18 13:20:43 +0800

    testing deploying

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
 
-app.listen(config.port,()=> {
+app.listen(process.env.PORT||config.port,()=> {
     connect();
 
     console.log(`server is listening ${config.baseUrl}`)
